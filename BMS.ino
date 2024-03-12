e3b69aab23e2c90f15182d88aa7a68f15d516890
Kiana Dumdumaya <kianadumdumaya@gmail.com>
2024-03-11 22:59:40 -0700

    Fix voltage sensors again
    Latest code that works with current BMS system. Current sensors not yet implemented.

--- BMS.ino
+++ BMS.ino
@@ -1,42 +1,60 @@
 // SENIOR DESIGN PROJECT: BMS PROTOTYPE
 
 // variables
-#define voltageSensor1 A4
-#define voltageSensor2 A5
+#define voltageSensor1 A1
+#define voltageSensor2 A2
+#define voltageSensor3 A3
+#define voltageSensor4 A4
+#define voltageSensor5 A5
+#define current A0
 
 #define discharge1 2
 #define discharge2 3
+#define discharge3 4
+#define discharge4 5
+#define discharge5 6
 
-int amountOfPacks = 0;  // This is the amount of variables in the array above.  This is calculated in setup.
+#define charger 8
+
+int amountOfPacks = 2;  // This is the amount of variables in the array above.  This is calculated in setup.
 int BV[] = { discharge1, discharge2 };
+const int BVsensors[] = { voltageSensor1, voltageSensor2 };
+const int BVoffset[] = { 0, 0 };
 float BVnum[2];
-
-// 3.99
-// 2.33
-
-
-
-// 3.98 - 3.98
-// 2.66 - 2.36
-
-
-const int charger = 8;
 
 float vout1 = 0.0;
 float vout2 = 0.0;
+float vout3 = 0.0;
+float vout4 = 0.0;
+float vout5 = 0.0;
+
 float vin1 = 0.0;
 float vin2 = 0.0;
+float vin3 = 0.0;
+float vin4 = 0.0;
+float vin5 = 0.0;
 
-float R1 = 30000.0;  // stock arduino resistors for voltage divider
-float R2 = 7500.0;
+const float R1 = 30000.0;  // stock arduino resistors for voltage divider
+const float R2 = 7500.0;
 
 float val1 = 0;
 float val2 = 0;
+float val3 = 0;
+float val4 = 0;
+float val5 = 0;
+
 float BV1 = 0;
 float BV2 = 0;
+float BV3 = 0;
+float BV4 = 0;
+float BV5 = 0;
 
 float BV1offset = -0.3;  // offsets for circuit variation, verify with voltmeter on cells
 float BV2offset = 0.0;
+float BV3offset = 0.0;
+float BV4offset = 0.0;
+float BV5offset = 0.0;
+
 float delayfactor = 0;
 
 void setup() {
@@ -51,30 +69,52 @@ void setup() {
   Serial.begin(9600);
 }
 
-// functions
+// FIXME: CURRENT SENSOR
+// int Sens = 66;  // Sensitivity in mV/A for the 30A version
+
+// const int analogIn = A0;  // Analog input pin
+
+// int OffsetVoltage = 2500;  // 0 Current offset Voltage in mV @ 0 amps
+
+// int RawValue = 0;    // Init result variables
+// double Voltage = 0;  
+// double Amps = 0;     
+
+// void currentSensors() {
+//   RawValue = analogRead(analogIn);            // Read voltage from ASC712
+//   Voltage = (RawValue / 1024.0) * 5000;       // Convert to mV
+//   Amps = ((Voltage - OffsetVoltage) / Sens);  // Convert to amps
+
+
+//   Serial.print("A/D Read Value = ");  // A/D read value
+//   Serial.print(RawValue);             //
+
+//   Serial.print("\t mV = ");  // ACS712 Output voltage
+//   Serial.print(Voltage, 3);  //
+
+//   Serial.print("\t Amps = ");  // Current measured
+//   Serial.println(Amps, 3);     //
+
+//   delay(2500);  // Wait a bit then do it again
+// }
+
 void read_voltages() {
-  // read all voltages
-  val1 = analogRead(voltageSensor1);
-  val2 = analogRead(voltageSensor2);
+  for (int i = 0; i < amountOfPacks; i++) {
+    // read all voltages
+    float val = analogRead(BVsensors[i]);
 
-  // do math
-  vout1 = (val1 * 5.0) / 1024.0;  // convert from digital to analog
-  vout2 = (val2 * 5.0) / 1024.0;
+    // do math
+    float vout = (val * 5.0) / 1024.0;  // convert from digital to analog
+    float vin = vout1 * (R1 + R2) / R2;
 
-  vin1 = vout1 * (R1 + R2) / R2;
-  vin2 = vout2 * (R1 + R2) / R2;
+    // Set battery voltage to vin plus offest
+    BVnum[i] = vin + BVoffset[i];
 
-  if (vin1 > vin2) {
-    BV1 = vin1 - vin2;
-    BV2 = vin2 + BV2offset;
-  } else {
-    BV1 = vin1 + BV1offset;
-    BV2 = vin2 - vin1;
+    // If the current index isn't 0, subtract the previous value from the current value
+    if (i != 0) {
+      BVnum[i] -= BVnum[i - 1];
+    }
   }
-  BV1 = 4.11;
-  BV2 = 4.9;
-  BVnum[0] = BV1;
-  BVnum[1] = BV2;
 }
 
 void print_voltages() {
@@ -82,42 +122,78 @@ void print_voltages() {
   Serial.print(BV1);
   Serial.print("   BV2: ");
   Serial.print(BV2);
+  // Serial.print("BV3: ");
+  // Serial.print(BV3);
+  // Serial.print("   BV4: ");
+  // Serial.print(BV4);
+  // Serial.print("BV5: ");
+  // Serial.print(BV5);
 
   Serial.print("      vin1: ");
   Serial.print(vin1);
   Serial.print("   vin2: ");
   Serial.print(vin2);
+  // Serial.print("      vin3: ");
+  // Serial.print(vin3);
+  // Serial.print("   vin4: ");
+  // Serial.print(vin4);
+  // Serial.print("      vin5: ");
+  // Serial.print(vin5);
 
+  // debugging: see voltage sensor raw values
   Serial.print("      val1: ");
   Serial.print(val1);
   Serial.print("   val2: ");
   Serial.println(val2);
+  // Serial.print("      val3: ");
+  // Serial.print(val3);
+  // Serial.print("   val4: ");
+  // Serial.println(val4);
+  // Serial.print("      val5: ");
+  // Serial.print(val5);
 }
 
-void digitalWriteAllDischargers(PinStatus input) {
-  for (int i = 0; i < amountOfPacks; i++) {
-    digitalWrite(BV[i], input);
-  }
-}
+// void digitalWriteAllDischargers(PinStatus init) {
+//   for (int i = 0; i < amountOfPacks; i++) {
+//     digitalWrite(BV[i], init);
+//   }
+// }
 
 int findLargestCell() {
-  /*
   int largestIndex = 0;
   for (int i = 1; i < amountOfPacks; i++) {
-    if (BV[i] > BV[largestIndex]) {
-      largestIndex = i;
+    // Check if the current index is bigger then the last recorded idnex
+    if (BVnum[i] > BVnum[largestIndex]) {
+      largestIndex = i;  // Update the biggest index if it is
     }
   }
-  */
 
-  return BV1 > BV2 ? 1 : 0;
+  return largestIndex;
+}
+
+bool isACellLargerThan(float input) {
+  for (int i = 0; i < amountOfPacks; i++) {
+    if (BVnum[i] > input)
+      return true;
+  }
+  return false;
+}
+
+bool isACellSmallerThan(float input) {
+  for (int i = 0; i < amountOfPacks; i++) {
+    if (BVnum[i] < input)
+      return true;
+  }
+  return false;
 }
 
 void loop() {
-  digitalWrite(charger, HIGH);
-  digitalWriteAllDischargers(LOW);
+  digitalWrite(charger, LOW);
+  // digitalWriteAllDischargers(LOW);
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
   read_voltages();
-  // print_voltages();
+  print_voltages();
 
   // change the delay factor depending on the balancing of cells; longer delay for very unbalanced cells
   if (abs(BV1 - BV2) > 1) {
@@ -132,78 +208,75 @@ void loop() {
 
   // DETERMINE IF CELLS NEED CHARGING OR DISCHARGING
   if (BV1 < 4.08 || BV1 > 4.12 || BV2 < 4.08 || BV2 > 4.12) {
-    Serial.println("Charging/Balancing in progress now, not all cells in acceptable range");
-    read_voltages();              // update voltage readings
-    digitalWrite(charger, HIGH);  // close charging relay
-
-    // DISCHARGE
-    if (BV1 > 4.10 || BV2 > 4.10) {
+    if (isACellLargerThan(4.12) || isACellSmallerThan(4.08)) {
+      Serial.println("Charging/Balancing in progress now, not all cells in acceptable range");
+      read_voltages();              // update voltage readings
       digitalWrite(charger, HIGH);  // close charging relay
 
+      // DISCHARGE
+      if (BV1 > 4.10 || BV2 > 4.10) {
+      float largestValue = 4.10;
+        if (isACellLargerThan(largestValue)) {
+          digitalWrite(charger, HIGH);  // close charging relay
 
-      int largestCellIndex = findLargestCell();  // Find which cell is the largest
 
-      // Is the largest cell larger than 4.10?
-      if (BVnum[largestCellIndex] > 4.10) {
-        Serial.print("Discharging Cell ");
-        Serial.println(largestCellIndex);
-        digitalWrite(BV[largestCellIndex], HIGH);  // open discharging relay
-        digitalWrite(charger, HIGH);               // close charging relay
+          int largestCellIndex = findLargestCell();  // Find which cell is the largest
+
+          // Is the largest cell larger than 4.10?
+          if (BVnum[largestCellIndex] > largestValue) {
+            Serial.print("Discharging Cell ");
+            Serial.println(largestCellIndex);
+            digitalWrite(BV[largestCellIndex], HIGH);  // open discharging relay
+            digitalWrite(charger, HIGH);               // close charging relay
+          }
+          delay(6000 * delayfactor);  // discharge for 6 seconds
+
+          // close discharging relays once done discharging
+          // digitalWriteAllDischargers(LOW);
+          digitalWrite(2, LOW);
+          digitalWrite(3, LOW);
+          digitalWrite(charger, HIGH);
+          Serial.println("Stabilizing voltage for 6 seconds");
+          delay(6000);
+
+          read_voltages();
+          Serial.println("Updated readings: ");
+          print_voltages();
+          delay(6000);
+        }
       }
-      delay(6000 * delayfactor);  // discharge for 6 seconds
 
-      /*
-      // check up on cell 1
-      if (BV1 > 4.10) {
-        Serial.println("Discharging Cell 1");
-        digitalWrite(discharge1, HIGH);  // open discharging relay
-        digitalWrite(charger, HIGH);     // close charging relay
+      // check if cells need charging and charge if so
+      if (BV1 <= 4.08 || BV2 <= 4.08) {
+        if (isACellSmallerThan(4.09)) {  // This was increased to 4.09 to handle getting rid of <= and instead using <
+          Serial.println("Charging now...");
+          read_voltages();
+          print_voltages();
+          delay(500);
+
+          // connect charger to circuit via relay #8 (charger)
+          // digitalWriteAllDischargers(LOW);
+          digitalWrite(2, LOW);
+          digitalWrite(3, LOW);
+          digitalWrite(charger, LOW);
+          delay(15000 * delayfactor);  // charge for this amount of time based on delay factor
+          digitalWrite(charger, HIGH);
+          Serial.println("Charging stopped. Letting cells stabilize...");
+          delay(6000);  // allow delay for cells to stabilize
+
+          read_voltages();
+          Serial.println("Updated readings: ");
+          print_voltages();
+        }
+
+        else {
+          // keep everything closed
+          digitalWrite(charger, HIGH);
+          // digitalWriteAllDischargers(LOW);
+          digitalWrite(2, LOW);
+          digitalWrite(3, LOW);
+        }
       }
-
-      // check up on cell 2
-      if (BV2 > 4.10) {
-        Serial.println("Discharging Cell 2");
-        digitalWrite(discharge2, HIGH);  // open discharging relay
-        digitalWrite(charger, HIGH);     // close charging relay
-      }
-      */
-
-      // close discharging relays once done discharging
-      digitalWriteAllDischargers(LOW);
-      digitalWrite(charger, HIGH);
-      Serial.println("Stabilizing voltage for 6 seconds");
-      delay(6000);
-
-      read_voltages();
-      Serial.println("Updated readings: ");
-      print_voltages();
-      delay(6000);
-    }
-
-    // check if cells need charging and charge if so
-    if (BV1 <= 4.08 || BV2 <= 4.08) {
-      Serial.println("Charging now...");
-      read_voltages();
-      print_voltages();
-      delay(500);
-
-      // connect charger to circuit via relay #8 (charger)
-      digitalWriteAllDischargers(LOW);
-      digitalWrite(charger, LOW);
-      delay(15000 * delayfactor);  // charge for this amount of time based on delay factor
-      digitalWrite(charger, HIGH);
-      Serial.println("Charging stopped. Letting cells stabilize...");
-      delay(6000);  // allow delay for cells to stabilize
-
-      read_voltages();
-      Serial.println("Updated readings: ");
-      print_voltages();
-    }
-
-    else {
-      // keep everything closed
-      digitalWrite(charger, HIGH);
-      digitalWriteAllDischargers(LOW);
     }
   }
 }
---
+++ test3/test3.ino
@@ -0,0 +1,225 @@
+// SENIOR DESIGN PROJECT: BMS PROTOTYPE
+
+// variables
+#define bot A2
+#define top A1
+#define currSensor A0
+
+#define discharge1 2
+#define discharge2 3
+#define charger 8
+
+int amountOfPacks = 0;  // This is the amount of variables in the array above.  This is calculated in setup.
+int BV[] = { discharge1, discharge2 };
+float BVnum[2];
+
+// initialized variables
+float adc_top = 0.0;
+float adc_bot = 0.0;
+float vin_top = 0.0;
+float vin_bot = 0.0;
+float vout_top = 0.0;
+float vout_bot = 0.0;
+float ref_voltage = 5.0;
+float BV1 = 0.0;
+float BV2 = 0.0;
+float wattage = 0.0;
+
+// voltage sensor values
+float R1 = 30000.0;  // stock arduino resistors for voltage divider
+float R2 = 7500.0;
+
+// current sensor values
+int sens = 66;
+int offsetVoltage = 2500;
+int rawVal = 0;
+double voltage = 0;
+double amps = 0; 
+
+float BV1offset = 0.0;  // offsets for circuit variation, verify with voltmeter on cells
+float BV2offset = 0.0;
+float delayfactor = 0.0;
+
+void setup() {
+  amountOfPacks = sizeof(BV) / sizeof(int);
+  for (int i = 0; i < amountOfPacks; i++) {
+    pinMode(BV[i], OUTPUT);
+  }
+
+  pinMode(charger, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+// functions
+void read_voltages() {
+  // Read the Analog Input  
+  adc_top = analogRead(top);
+  adc_bot = analogRead(bot);
+  
+  // Determine voltage at ADC input
+  vout_bot = (adc_bot * ref_voltage) / 1024.0;
+  vout_top = (adc_top * ref_voltage) / 1024.0;
+  
+  // Calculate voltage at divider input
+  vin_bot = vout_bot*(R1+R2)/R2;
+  vin_top = abs((vout_top*(R1+R2)/R2)-vin_bot);
+
+  vin_bot = vin_bot + 1.5;
+  vin_top = vin_top + 2;
+
+  // Print results to Serial Monitor to 2 decimal places
+  Serial.print("bot voltage = ");
+  Serial.print(vin_bot, 2);
+
+  Serial.print("      top voltage = ");
+  Serial.println(vin_top, 2);
+  
+  BV1 = vin_top;
+  BV2 = vin_bot;
+  BV[0] = BV1;
+  BV[1] = BV2;
+
+  delay(500);
+}
+
+// void print_voltages() {
+//   Serial.print("BV1: ");
+//   Serial.print(BV1);
+//   Serial.print("   BV2: ");
+//   Serial.print(BV2);
+
+//   Serial.print("      vin1: ");
+//   Serial.print(vin1);
+//   Serial.print("   vin2: ");
+//   Serial.print(vin2);
+
+//   Serial.print("      vout1: ");
+//   Serial.print(vout1);
+//   Serial.print("   vout2: ");
+//   Serial.print(vout2);
+
+//   Serial.print("      val1: ");
+//   Serial.print(val1);
+//   Serial.print("   val2: ");
+//   Serial.println(val2);
+// }
+
+void digitalWriteAllOn() {
+  for (int i = 0; i < amountOfPacks; i++) {
+    digitalWrite(BV[i], HIGH);
+  }
+}
+
+void digitalWriteAllOff() {
+  for (int i = 0; i < amountOfPacks; i++) {
+    digitalWrite(BV[i], LOW);
+  }
+}
+
+int findLargestCell() {
+  int largestIndex = 0;
+  for (int i = 1; i < amountOfPacks; i++) {
+    if (BV[i] > BV[largestIndex]) {
+      largestIndex = i;
+    }
+  }
+
+  return largestIndex;
+}
+
+void loop() {
+  digitalWrite(charger, HIGH);
+  digitalWriteAllOff();
+  read_voltages();
+  // print_voltages();
+
+  // change the delay factor depending on the balancing of cells; longer delay for very unbalanced cells
+  if (abs(BV1 - BV2) > 1) {
+    delayfactor = 6;
+  } else if (abs(BV1 - BV2) > 0.5 && abs(BV1 - BV2) < -1) {
+    delayfactor = 4;
+  } else if (abs(BV1 - BV2) > 0.2 && abs(BV1 - BV2) <= 0.5) {
+    delayfactor = 2;
+  } else {
+    delayfactor = 0.5;
+  }
+  // delayfactor = 0.5;
+
+  // DETERMINE IF CELLS NEED CHARGING OR DISCHARGING
+  if (BV1 < 4.08 || BV1 > 4.12 || BV2 < 4.08 || BV2 > 4.12) {
+    Serial.println("Charging/Balancing in progress now, not all cells in acceptable range");
+    read_voltages();              // update voltage readings
+    digitalWrite(charger, HIGH);  // close charging relay
+
+    // DISCHARGE
+    if (BV1 > 4.10 || BV2 > 4.10) {
+      digitalWrite(charger, HIGH);  // close charging relay
+
+
+      int largestCellIndex = findLargestCell();  // Find which cell is the largest
+
+      // Is the largest cell larger than 4.10?
+      if (BVnum[largestCellIndex] > 4.10) {
+        Serial.print("Discharging Cell ");
+        Serial.println(largestCellIndex);
+        digitalWrite(BV[largestCellIndex], HIGH);  // open discharging relay
+        digitalWrite(charger, HIGH);               // close charging relay
+      }
+      delay(6000 * delayfactor);  // discharge for 6 seconds
+
+      /*
+      // check up on cell 1
+      if (BV1 > 4.10) {
+        Serial.println("Discharging Cell 1");
+        digitalWrite(discharge1, HIGH);  // open discharging relay
+        digitalWrite(charger, HIGH);     // close charging relay
+      }
+
+      // check up on cell 2
+      if (BV2 > 4.10) {
+        Serial.println("Discharging Cell 2");
+        digitalWrite(discharge2, HIGH);  // open discharging relay
+        digitalWrite(charger, HIGH);     // close charging relay
+      }
+      */
+
+      // close discharging relays once done discharging
+      digitalWriteAllOff();
+      digitalWrite(charger, HIGH);
+      Serial.println("Stabilizing voltage for 6 seconds");
+      delay(6000);
+
+      Serial.println("Updated readings: ");
+      read_voltages();
+      // print_voltages();
+      delay(6000);
+    }
+
+    // check if cells need charging and charge if so
+    if (BV1 <= 4.08 || BV2 <= 4.08) {
+      Serial.println("Charging now...");
+      read_voltages();
+      // print_voltages();
+      delay(500);
+
+      // connect charger to circuit via relay #8 (charger)
+      digitalWriteAllOff();
+      digitalWrite(charger, LOW);
+      delay(15000 * delayfactor);  // charge for this amount of time based on delay factor
+      digitalWrite(charger, HIGH);
+      Serial.println("Charging stopped. Letting cells stabilize...");
+      delay(6000);  // allow delay for cells to stabilize
+
+      Serial.println("Updated readings: ");
+      read_voltages();
+      // print_voltages();
+    }
+
+    else {
+      // keep everything closed
+      digitalWrite(charger, HIGH);
+      digitalWriteAllOff();
+    }
+  }
+}
